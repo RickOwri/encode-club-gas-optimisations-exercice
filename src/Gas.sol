72ad46789226dddd3eff5b84d6febc8b51b75322
Mickael <mickael.rutembya@gmail.com>
2023-08-07 19:55:40 +0200

    round1

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,40 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-import "./Ownable.sol";
+// import "./Ownable.sol";
 
-
-
-contract GasContract is Ownable {
+contract GasContract {
     uint256 private totalSupply; // cannot be updated
     uint256 private paymentCounter = 0;
     address[5] public administrators;
     bool private wasLastOdd = true;
 
     mapping(address => uint256) public balances;
-    mapping(address => mapping(uint256 => Payment)) private payments;
+    // mapping(address => mapping(uint256 => Payment)) private payments;
     mapping(address => uint256) public whitelist;
     mapping(address => bool) private isOddWhitelistUser;
     mapping(address => ImportantStruct) private whiteListStruct;
 
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-    
-    struct Payment {
-        uint256 amount;
-        address recipient;
-        bool adminUpdated;
-        PaymentType paymentType;
-        address admin; // administrators address
-        // TODO: use bytes32 instead
-        string recipientName; // max 8 characters 
-    }
-    
+    // enum PaymentType {
+    // Unknown,
+    // BasicPayment,
+    // Refund,
+    // Dividend,
+    // GroupPayment
+    // }
+
+    // struct Payment {
+    // uint256 amount;
+    // address recipient;
+    // bool adminUpdated;
+    // PaymentType paymentType;
+    // address admin; // administrators address
+    // TODO: use bytes32 instead
+    // string recipientName; // max 8 characters
+    // }
+
     struct ImportantStruct {
         uint256 amount;
         uint256 valueA; // max 3 digits
@@ -45,19 +43,10 @@ contract GasContract is Ownable {
     }
 
     modifier checkIfWhiteListed(address sender) {
-        require(
-            msg.sender == sender,
-            "originator not sender"
-        );
+        require(msg.sender == sender, "originator not sender");
         uint256 usersTier = whitelist[msg.sender];
-        require(
-            usersTier > 0,
-            "user not whitelisted"
-        );
-        require(
-            usersTier < 4,
-            "usersTier invalid"
-        );
+        require(usersTier > 0, "user not whitelisted");
+        require(usersTier < 4, "usersTier invalid");
         _;
     }
 
@@ -100,88 +89,76 @@ contract GasContract is Ownable {
         return false;
     }
 
-
     function balanceOf(address _user) external view returns (uint256 balance_) {
         return balances[_user];
     }
-
 
     function transfer(
         address _recipient,
         uint256 _amount,
         // TODO: use bytes32 instead
         string calldata _name
-    ) public returns (bool status_) {
-        require(
-            balances[msg.sender] >= _amount,
-            "Sender insufficient Balance"
-        );
+    ) public {
+        require(balances[msg.sender] >= _amount, "Sender insufficient Balance");
         require(
             bytes(_name).length < 9,
             "recipient name is too long, max length 8"
         );
         balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
-        emit Transfer(_recipient, _amount);
-        payments[msg.sender][paymentCounter].admin = address(0);
-        payments[msg.sender][paymentCounter].adminUpdated = false;
-        payments[msg.sender][paymentCounter].paymentType = PaymentType.BasicPayment;
-        payments[msg.sender][paymentCounter].recipient = _recipient;
-        payments[msg.sender][paymentCounter].amount = _amount;
-        payments[msg.sender][paymentCounter].recipientName = _name;
+        // emit Transfer(_recipient, _amount);
+        // payments[msg.sender][paymentCounter].admin = address(0);
+        // payments[msg.sender][paymentCounter].adminUpdated = false;
+        // payments[msg.sender][paymentCounter].paymentType = PaymentType.BasicPayment;
+        // payments[msg.sender][paymentCounter].recipient = _recipient;
+        // payments[msg.sender][paymentCounter].amount = _amount;
+        // payments[msg.sender][paymentCounter].recipientName = _name;
 
-        return true;
+        // return true;
     }
 
-    function updatePayment(
-        address _user,
-        uint256 _ID,
-        uint256 _amount,
-        PaymentType _type
-    ) external {
-        // require(
-        //     checkForAdmin(msg.sender),
-        //     "Caller not admin"
-        // );
-        // require(
-        //     _ID > 0,
-        //     "ID must be greater than 0"
-        // );
-        // require(
-        //     _amount > 0,
-        //     "Amount must be greater than 0"
-        // );
-        // require(
-        //     _user != address(0),
-        //     "Admin must have a valid address"
-        // );
+    // function updatePayment(
+    // address _user,
+    // uint256 _ID,
+    // uint256 _amount,
+    // PaymentType _type
+    // ) external {
+    // require(
+    //     checkForAdmin(msg.sender),
+    //     "Caller not admin"
+    // );
+    // require(
+    //     _ID > 0,
+    //     "ID must be greater than 0"
+    // );
+    // require(
+    //     _amount > 0,
+    //     "Amount must be greater than 0"
+    // );
+    // require(
+    //     _user != address(0),
+    //     "Admin must have a valid address"
+    // );
 
-        payments[_user][_ID].adminUpdated = true;
-        payments[_user][_ID].admin = _user;
-        payments[_user][_ID].paymentType = _type;
-        payments[_user][_ID].amount = _amount;
+    //     payments[_user][_ID].adminUpdated = true;
+    //     payments[_user][_ID].admin = _user;
+    //     payments[_user][_ID].paymentType = _type;
+    //     payments[_user][_ID].amount = _amount;
 
-        emit PaymentHistory(_user);
-        emit PaymentUpdated(
-            msg.sender,
-            _ID,
-            _amount,
-            payments[_user][_ID].recipientName
-        );
-    }
+    //     emit PaymentHistory(_user);
+    //     emit PaymentUpdated(
+    //         msg.sender,
+    //         _ID,
+    //         _amount,
+    //         payments[_user][_ID].recipientName
+    //     );
+    // }
 
-    function addToWhitelist(address _userAddrs, uint256 _tier)
-        external
-    {
-        require(
-            checkForAdmin(msg.sender),
-            "Caller not admin"
-        );
-        require(
-            _tier < 255,
-            "tier level greater than 255"
-        );
-        
+    function addToWhitelist(address _userAddrs, uint256 _tier) external {
+        // keep
+        require(checkForAdmin(msg.sender), "Caller not admin");
+        require(_tier < 255, "tier level greater than 255");
+        // keep
         if (_tier > 3) {
             whitelist[_userAddrs] = 3;
         } else if (_tier > 0 && _tier < 3) {
@@ -189,15 +166,16 @@ contract GasContract is Ownable {
         } else {
             whitelist[_userAddrs] = _tier;
         }
-        
 
-        if (wasLastOdd) {
-            isOddWhitelistUser[_userAddrs] = wasLastOdd;
-            wasLastOdd = false;
-        } else {
-            isOddWhitelistUser[_userAddrs] = wasLastOdd;
-            wasLastOdd = true;
-        }
+        // if (wasLastOdd) {
+        //     isOddWhitelistUser[_userAddrs] = wasLastOdd;
+        //     wasLastOdd = false;
+        // } else {
+        //     isOddWhitelistUser[_userAddrs] = wasLastOdd;
+        //     wasLastOdd = true;
+        // }
+
+        // keep
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
@@ -205,25 +183,36 @@ contract GasContract is Ownable {
         address _recipient,
         uint256 _amount
     ) external checkIfWhiteListed(msg.sender) {
-        whiteListStruct[msg.sender] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
-        
-        require(
-            balances[msg.sender] >= _amount,
-            "Sender has insufficient Balance"
+        whiteListStruct[msg.sender] = ImportantStruct(
+            _amount,
+            0,
+            0,
+            0,
+            true,
+            msg.sender
         );
-        require(
-            _amount > 3,
-            "amount to send have to be bigger than 3"
-        );
-        balances[msg.sender] -= (_amount- whitelist[msg.sender]);
+
+        // require(
+        //     balances[msg.sender] >= _amount,
+        //     "Sender has insufficient Balance"
+        // );
+        // require(
+        //     _amount > 3,
+        //     "amount to send have to be bigger than 3"
+        // );
+        balances[msg.sender] -= (_amount - whitelist[msg.sender]);
         balances[_recipient] += (_amount - whitelist[msg.sender]);
-        
+
         emit WhiteListTransfer(_recipient);
     }
 
-
-    function getPaymentStatus(address sender) external view returns (bool, uint256) {
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+    function getPaymentStatus(
+        address sender
+    ) external view returns (bool, uint256) {
+        return (
+            whiteListStruct[sender].paymentStatus,
+            whiteListStruct[sender].amount
+        );
     }
 
     receive() external payable {
